21f7dd50b7f162a1095c6f4617602aec6c2918f9
aniketjha9179 <aniketjha917928@gmail.com>
2024-07-03 18:09:08 +0530

    added register user validation

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import globaleErrorHanler from "./middlewares/globalErrorhandler";
 import userRouter from "./user/userRouter";
 
 const app = express();
+app.use(express.json())
 // routes
 app.get("/", (req, res, next) => {
   const error=createHttpError(400,"Something went wrong")
--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,13 +1,19 @@
 import { Request, Response, NextFunction } from "express";
+import createHttpError from "http-errors";
 
-const createUser = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-    res.json({
-        message:'User creted'
-    })
+const createUser = async (req: Request, res: Response, next: NextFunction) => {
+  const { name, email, password } = req.body;
+  // validation
+
+  if (!name || !email || !password) {
+    const error = createHttpError(400, "All fields are requied");
+    return next(error);
+  }
+
+  // process
+  
+  // response
+  res.json({ message: "User creted" });
 };
 
 export { createUser };
